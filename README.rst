97d1cb7c16005870ce1ab5cbddfd273629354c1d
Bradley Ayers <bradley.ayers@gmail.com>
2012-08-31 13:46:11 +1000

    Add auth token caching to ClientLoginAuth, bump to v0.2.0

--- README.rst
+++ README.rst
@@ -12,3 +12,17 @@ Usage::
     {...}
     >>> submit_job(printer="0e506d12-dbe0-54d3-7392-fd69d45ff2fc", content="test.pdf", auth=auth)
     {...}
+
+
+Change log
+==========
+
+0.2.0
+=====
+
+- Make `ClientLoginAuth` cache authentication token
+
+0.1.0
+=====
+
+- Initial version
--- cloudprinting/auth.py
+++ cloudprinting/auth.py
@@ -1,12 +1,49 @@
+import threading
+import requests
 from .sample import GetAuthTokens
 
 
 class ClientLoginAuth(object):
-    def __init__(self, email, password):
+    """
+    Authenticates a request using Google's deprecated Client Login API.
+
+    :param caching: cache the authentication token across requests
+
+    When authentication token caching is enabled, the request will be retried
+    once if the response status is 'Forbidden'. This handles expired
+    authentication tokens.
+    """
+    def __init__(self, email, password, caching=True):
         self.email = email
         self.password = password
+        self.caching = caching
+        self.lock = threading.Lock()
 
     def __call__(self, r):
-        tokens = GetAuthTokens(self.email, self.password)
-        r.headers['Authorization'] = 'GoogleLogin auth=%s' % tokens['Auth']
+        r.headers['Authorization'] = 'GoogleLogin auth=%s' % self.token
+
+        if self.caching:
+            # Hook the response to retry if the auth token is stale
+            def hook(response):
+                if response.status_code == requests.codes.forbidden:
+                    del self.token  # clear stale token
+                    request = response.request
+                    request.deregister_hook('response', hook)  # retry one time
+                    request.send(anyway=True)
+                    return request.response
+                return response
+            r.hooks['response'].insert(0, hook)
         return r
+
+    @property
+    def token(self):
+        with self.lock:
+            if not self.caching or not hasattr(self, "_token"):
+                self._token = GetAuthTokens(self.email, self.password)["Auth"]
+                return self._token
+            return self._token
+
+    @token.deleter
+    def token(self):
+        with self.lock:
+            del self._token
